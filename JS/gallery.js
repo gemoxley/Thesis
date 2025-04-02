1961fcd7f985b2856897ec219058414268f648f2
gemoxley <gmoxley@dons.usfca.edu>
2025-04-01 17:39:32 -0700

    Push

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,134 +1,134 @@
 let frameImages = [];
 let contentImages = [];
 let frames = [];
-let scrollSpeed = 2.5;
-let gap = 50;
-let lastFrameImg = null;
-let lastContentImg = null;
+const FRAME_HEIGHT = 600;
+const FRAME_WIDTH = 400;
+const ROWS = 2;
+const COLS = 4;
+const SCROLL_SPEED = 1.5;
 
 function preload() {
-  frameImages.push(loadImage('images/frame-1.png'));
-  frameImages.push(loadImage('images/frame-2.png'));
-  frameImages.push(loadImage('images/frame-3.png'));
-  frameImages.push(loadImage('images/frame-4.png'));
-  frameImages.push(loadImage('images/frame-5.png'));
-  frameImages.push(loadImage('images/frame-6.png'));
-  contentImages.push(loadImage('images/Intestinal-Diagram.png'));
-  contentImages.push(loadImage('images/Sympathetic-Man.png'));
-  contentImages.push(loadImage('images/Hand-Radiograph.png'));
-  contentImages.push(loadImage('images/Kaishi-Hen-Ribs.png'));
-  contentImages.push(loadImage('images/Arm-Muscle-Ecorche.png'));
-  contentImages.push(loadImage('images/Ribs-Pelvis-Diagram.png'));
-  contentImages.push(loadImage('images/Somatometric-Chart-Bone-Lengths.png'));
-  console.log('Frame images loaded:', frameImages);
-  console.log('Content images loaded:', contentImages);
+  for (let i = 1; i <= 6; i++) {
+    frameImages.push(loadImage(`images/frame-${i}.png`));
+  }
+  const contentFiles = [
+    'Intestinal-Diagram.png',
+    'Sympathetic-Man.png',
+    'Hand-Radiograph.png',
+    'Kaishi-Hen-Ribs.png',
+    'Arm-Muscle-Ecorche.png',
+    'Ribs-Pelvis-Diagram.png',
+    'Somatometric-Chart-Bone-Lengths.png'
+  ];
+  contentFiles.forEach(file => {
+    contentImages.push(loadImage(`images/${file}`));
+  });
 }
 
 function setup() {
-  const canvas = createCanvas(windowWidth, windowHeight);
+  const canvas = createCanvas(windowWidth, FRAME_HEIGHT * ROWS);
   canvas.parent('canvas-container');
-  console.log('Canvas created:', canvas);
-  shuffleArray(frameImages);
-  shuffleArray(contentImages);
-  let x = 0;
-  while (x < width) {
-    addNewFrame(x);
-    x += frames[frames.length - 1].w + gap;
-  }
+  shuffleArrays();
+  initializeGrid();
 }
 
 function draw() {
-  background(240);
+  clear();
+  for (let frame of frames) {
+    frame.update();
+    frame.display();
+  }
+  recycleFrames();
+}
+
+function initializeGrid() {
+  const totalFrames = ROWS * (COLS + 2);
+  for (let i = 0; i < totalFrames; i++) {
+    const row = Math.floor(i / (COLS + 2));
+    const col = i % (COLS + 2);
+    addNewFrame(
+      col * FRAME_WIDTH,
+      row * FRAME_HEIGHT
+    );
+  }
+}
+
+function addNewFrame(x, y) {
+  const frameImg = random(frameImages);
+  const contentImg = random(contentImages);
+  const padding = 20;
+  const maxContentWidth = FRAME_WIDTH - padding * 2;
+  const maxContentHeight = FRAME_HEIGHT - padding * 2;
+  let contentWidth, contentHeight;
+  const contentRatio = contentImg.width / contentImg.height;
+  if (contentRatio > 1) {
+    contentWidth = maxContentWidth;
+    contentHeight = contentWidth / contentRatio;
+    if (contentHeight > maxContentHeight) {
+      contentHeight = maxContentHeight;
+      contentWidth = contentHeight * contentRatio;
+    }
+  } else {
+    contentHeight = maxContentHeight;
+    contentWidth = contentHeight * contentRatio;
+    if (contentWidth > maxContentWidth) {
+      contentWidth = maxContentWidth;
+      contentHeight = contentWidth / contentRatio;
+    }
+  }
+  
+  frames.push(new Frame(
+    x,
+    y,
+    FRAME_WIDTH,
+    FRAME_HEIGHT,
+    frameImg,
+    contentImg,
+    contentWidth,
+    contentHeight,
+    padding
+  ));
+}
+
+function recycleFrames() {
   for (let i = frames.length - 1; i >= 0; i--) {
-    frames[i].update();
-    frames[i].display();
-    if (frames[i].isOffScreen()) {
-      frames.splice(i, 1);
-      let lastFrame = frames.length > 0 ? frames[frames.length - 1] : null;
-      let x = lastFrame ? lastFrame.x + lastFrame.w + gap : width;
-      addNewFrame(x);
+    if (frames[i].x + FRAME_WIDTH < -50) {
+     const farthestRight = frames.reduce((max, frame) => 
+        Math.max(max, frame.x), -Infinity);
+      frames[i].x = farthestRight + FRAME_WIDTH;
+     frames[i].y = frames[i].y;
+      frames[i].frameImg = random(frameImages);
+     frames[i].contentImg = random(contentImages);
     }
   }
 }
 
-function addNewFrame(x) {
-  let frameImg, contentImg;
-  do {
-    frameImg = random(frameImages);
-  } while (frameImg === lastFrameImg);
-  lastFrameImg = frameImg;
-  do {
-    contentImg = random(contentImages);
-  } while (contentImg === lastContentImg);
-  lastContentImg = contentImg;
-  let frameHeight = height;
-  let frameWidth = (frameHeight * frameImg.width) / frameImg.height;
-  let imgWidth = frameWidth;
-  let imgHeight = (frameWidth * contentImg.height) / contentImg.width;
-  if (imgHeight > frameHeight) {
-    imgHeight = frameHeight;
-    imgWidth = (frameHeight * contentImg.width) / contentImg.height;
-  }
-  frames.push(new ImageFrame(x, 0, frameWidth, frameHeight, frameImg, contentImg, imgWidth, imgHeight));
-  console.log('Frame added at x:', x, 'with size:', frameWidth, frameHeight);
-}
-
-class ImageFrame {
-  constructor(x, y, w, h, frameImg, contentImg, imgWidth, imgHeight) {
+class Frame {
+  constructor(x, y, w, h, frameImg, contentImg, contentW, contentH, padding) {
     this.x = x;
     this.y = y;
-    this.w = w;
-    this.h = h;
+    this.width = w;
+    this.height = h;
     this.frameImg = frameImg;
     this.contentImg = contentImg;
-    this.imgWidth = imgWidth;
-    this.imgHeight = imgHeight;
+    this.contentWidth = contentW;
+    this.contentHeight = contentH;
+    this.padding = padding;
   }
   update() {
-    this.x -= scrollSpeed;
+    this.x -= SCROLL_SPEED;
   }
   display() {
-    if (this.contentImg && this.frameImg) {
-      let contentX = this.x + (this.w - this.imgWidth) / 2;
-      let contentY = this.y + (this.h - this.imgHeight) / 2;
-      image(this.contentImg, contentX, contentY, this.imgWidth, this.imgHeight);
-      image(this.frameImg, this.x, this.y, this.w, this.h);
-      console.log('Frame displayed at:', this.x, this.y, 'with size:', this.w, this.h);
-      class ImageFrame {
-        constructor(x, y, w, h, frameImg, contentImg, imgWidth, imgHeight) {
-          this.x = x;
-          this.y = y;
-          this.w = w;
-          this.h = h;
-          this.frameImg = frameImg;
-          this.contentImg = contentImg;
-          this.imgWidth = imgWidth;
-          this.imgHeight = imgHeight;
-        }
-        update() {
-          this.x -= scrollSpeed;
-        }
-        display() {
-          if (this.contentImg && this.frameImg) {
-            let contentX = this.x + (this.w - this.imgWidth) / 2;
-            let contentY = this.y + (this.h - this.imgHeight) / 2;
-            push();
-            noStroke();
-            rect(this.x, this.y, this.w, this.h);
-            image(this.contentImg, contentX, contentY, this.imgWidth, this.imgHeight);
-            pop();
-            image(this.frameImg, this.x, this.y, this.w, this.h);
-          }
-        }
-        isOffScreen() {
-          return this.x + this.w < -preloadOffset;
-        }
-      }      
-    }
+    const contentX = this.x + (this.width - this.contentWidth) / 2;
+    const contentY = this.y + (this.height - this.contentHeight) / 2;
+    image(this.contentImg, contentX, contentY, this.contentWidth, this.contentHeight);
+    image(this.frameImg, this.x, this.y, this.width, this.height);
   }
-  isOffScreen() {
-    return this.x + this.w < 0;
-  }
+}
+
+function shuffleArrays() {
+  shuffleArray(frameImages);
+  shuffleArray(contentImages);
 }
 
 function shuffleArray(array) {
@@ -139,12 +139,5 @@ function shuffleArray(array) {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  console.log('Canvas resized to:', windowWidth, windowHeight);
-  frames = [];
-  let x = 0;
-  while (x < width) {
-    addNewFrame(x);
-    x += frames[frames.length - 1].w + gap;
-  }
+  resizeCanvas(windowWidth, FRAME_HEIGHT * ROWS);
 }
