f2e264a07108c332baf2b8896f318102c100d24c
gemoxley <gmoxley@dons.usfca.edu>
2025-04-21 19:15:08 -0700

    h

--- JS/gallery.js
+++ JS/gallery.js
@@ -211,4 +211,12 @@ function shuffleArrays() {
 }
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.fl
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+function windowResized() {
+  resizeCanvas(windowWidth, FRAME_HEIGHT);
+  frames = [];
+  initializeGrid();
+}
